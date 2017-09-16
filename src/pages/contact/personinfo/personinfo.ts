29152d5c789254b85f98a6d4231beaf8e3e6ca0c
BadFlag <songjiaqi_s@163.com>
2017-09-16 11:06:52 +0800

    完善登录时本地用户信息存储功能， 完成退出登录功能， 修改个人信息显示页面

--- src/pages/contact/personinfo/personinfo.ts
+++ src/pages/contact/personinfo/personinfo.ts
@@ -8,4 +8,16 @@ import {BaseComponent} from "../../../components/base/base";
   templateUrl: 'personinfo.html'
 })
 export class PersonInfoPage extends BaseComponent {
+
+  private mUserName: string = this.userData.getUserName();
+
+  private mCompanyName: string = this.userData.getCompanyName();
+
+  private mRole: string = this.userData.getRole();
+
+  private mCompanyCode: string = this.userData.getCompanyCode();
+
+  private mUserId: string = this.userData.getUserId();
+
+
 }
