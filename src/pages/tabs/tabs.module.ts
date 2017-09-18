67b8157e83a2684a90126d8a35ae90a4f2afe066
王岩 <Wyan_7@163.com>
2017-09-18 14:28:11 +0800

    权限管理

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
 import { TabsPage } from './tabs';
+import {DirectivesModule} from "../../directives/directives.module";
 
 @NgModule({
     imports: [
-        IonicPageModule.forChild(TabsPage)
+        IonicPageModule.forChild(TabsPage),
+        DirectivesModule
     ],
     declarations: [TabsPage],
     entryComponents: [TabsPage]
